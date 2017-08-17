d06a26dacf3a7bb39d2b90c5b5947a0099946959
Gryazev Dmitry <gryazevdv@yandex.ru>
2017-08-17 12:31:40 +0300

    create array continent

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo "привет";
+$continents = [
+  'Africa' => [
+    'Elephantidae',
+    'Giraffa camelopardalis',
+    'Hippotigris',
+    'Panthera leo',
+    'Tropicranus albocristatus'
+  ],
+  'Australia' => [
+    'Tachyglossus aculeatus',
+    'Canis dingo',
+    'Macropus',
+    'Phascolarctos cinereus',
+    'Bufo marinus'
+  ]
+];
+
+foreach ($continents as $continent => $animals) {
+	foreach ($animals as $animalKey => $animal) {
+		echo $continent . ' - ' . $animal . PHP_EOL;	
+	}
+}
