64e8573d5541377590188e4bf3bff3c1e2ca1cda
Gryazev Dmitry <gryazevdv@yandex.ru>
2017-08-17 15:15:22 +0300

    array with new animals

--- index.php
+++ index.php
@@ -23,16 +23,57 @@ $continents = [
 	}
 }*/
 
-$new_array = [];
+$two_words_animals = [];   // массив со всеми животными из двух слов
+$two_words_animals1 = [];  // массив с первыми словами
+$two_words_animals2 = [];  // массив со вторыми словами
 
+
+//  ищу животных из двух слов
 foreach ($continents as $continent => $animals) {
 	for ($i=0; $i < count($animals); $i++) {
 		if (substr_count($animals[$i], ' ') == 1 ) {
-			$new_array[] = $animals[$i];
+			$two_words_animals[] = $animals[$i];
 		}
 	}
 }
 
 echo '<pre>';
-print_r($new_array);
+print_r($two_words_animals);
 echo '</pre>';
+
+$string = implode(' ', $two_words_animals);  // преобразовал массив в строку
+echo $string;
+
+$array = explode(' ', $string); 						 // из строки в массив по одному слову
+echo '<pre>';
+print_r($array);
+echo '</pre>';
+
+for ($i = 0; $i < count($array); $i+=2) {    // первое слово животного в массив
+	$two_words_animals1[] = $array[$i]; 
+}
+
+echo '<pre>';
+shuffle($two_words_animals1);
+print_r($two_words_animals1);
+echo '</pre>';
+
+for ($i = 1; $i < count($array); $i+=2) {    // второе слово животного в массив
+	$two_words_animals2[] = $array[$i]; 
+}
+
+echo '<pre>';
+shuffle($two_words_animals2);
+print_r($two_words_animals2);
+echo '</pre>';
+
+$result = [];																 // итовый массив с фантастическими животными
+
+for ($i = 0; $i < count($two_words_animals1); $i++) {
+	$result[$i] = $two_words_animals1[$i] . ' ' . $two_words_animals2[$i];
+}
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
+
