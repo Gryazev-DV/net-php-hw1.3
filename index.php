c2c9c4095dd6b70327878297a34f3d2e71438d1d
Gryazev Dmitry <gryazevdv@yandex.ru>
2017-08-17 14:14:10 +0300

    new array animals with two words

--- index.php
+++ index.php
@@ -17,8 +17,22 @@ $continents = [
   ]
 ];
 
-foreach ($continents as $continent => $animals) {
+/*foreach ($continents as $continent => $animals) {
 	foreach ($animals as $animalKey => $animal) {
 		echo $continent . ' - ' . $animal . PHP_EOL;	
 	}
+}*/
+
+$new_array = [];
+
+foreach ($continents as $continent => $animals) {
+	for ($i=0; $i < count($animals); $i++) {
+		if (substr_count($animals[$i], ' ') == 1 ) {
+			$new_array[] = $animals[$i];
+		}
+	}
 }
+
+echo '<pre>';
+print_r($new_array);
+echo '</pre>';
